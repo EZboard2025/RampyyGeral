82ed7f3344967db8ae1a2532f3553af827a135a1
EZboard2025 <aixconectawkn@gmail.com>
2025-08-12 18:54:00 -0300

    Atualiza os manifestos de construção e caminhos da aplicação, reintroduzindo a página "/page". Modifica o hash do webpack e atualiza arquivos de cache, garantindo a integridade do processo de build.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2951e505810b4ee2"; }
+/******/ 		__webpack_require__.h = function() { return "c68039967e56913f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
